b8fb170c097ce7f28502bba0df0699468eb5d002
Paul <paul.kiplagt@student.moringaschool.com>
2022-08-18 16:14:46 +0300

    second

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,3 +7,13 @@ class Triangle
     @b = b
     @c = c
 end
+def kind
+  validate_triangle
+  if a == b && b == c
+    :equilateral
+  elsif a == b || b == c || a == c
+    :isosceles
+  else
+    :scalene
+  end
+end
